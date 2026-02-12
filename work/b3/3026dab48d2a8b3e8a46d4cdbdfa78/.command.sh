f7bb7334f8cb856e6910a0da732d85138e627b52
miguelrosell <miguelnorty@gmail.com>
2026-02-12 02:18:28 +0100

    Delete work/b3/3026dab48d2a8b3e8a46d4cdbdfa78 directory

--- work/b3/3026dab48d2a8b3e8a46d4cdbdfa78/.command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ue
-Rscript /home/miguelito/Documentos/module_cosimflow/tests/bin/compute_cosine.R \
-    --input test_expression.csv \
-    --out_prefix similarity \
-    --method cosine \
-    --min_gene_mean 0.0 \
-    --sample_cols "auto"
